820ec829e699610ef69788ba92d572c6a2cb5308
edilanesilva <edilanesantos75@gmail.com>
2023-05-02 20:41:35 -0300

    feat:apply theme colors to page elements

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,37 +1,63 @@
-const html = document.querySelector('html')
-const checkbox = document.querySelector('#dark_mode')
-/* pega o estilo */
+const html = document.querySelector('html');
+const topHeader = document.querySelector('.background-box');
+const checkbox = document.querySelector('#dark_mode');
+const body = document.querySelector('body');
+const cards = document.querySelectorAll(".card")
+const numbers = document.querySelectorAll('[data-number]')
+const title = document.querySelectorAll('[data-title]')
 
+
+/* pega o estilo */
 const getStyle = (element, style) =>
     window.getComputedStyle(element).getPropertyValue(style)
 
-
-
 const lightTheme = {
     bg: getStyle(html, "--white-bg"),
-    topHeaderBg: getStyle(html, "--veryPaleBlue-topbg"),
+    topHeaderBg: getStyle(html, "--top-header-bg"),
     cardBg: getStyle(html, "--cardbg"),
     colorText: getStyle(html, "--darkGrayishBlue-text"),
-    cardText: getStyle(html, "--veryDarkBlue-text")
+    cardText: getStyle(html, "--veryDarkBlue-text"),
+    selected: getStyle(html, "--light-selected")
 }
 
 const darkTheme = {
     bg: getStyle(html, "--dark-blue-bg"),
     topHeaderBg: getStyle(html, "--dark-blue-topbg"),
     cardBg: getStyle(html, "--dark-blue-cardbg"),
-    colorText: getStyle(html, "--blue-text"),
-    cardText: getStyle(html, "--white-text")
+    colorText: getStyle(html, "--white-bg"),
+    cardText: getStyle(html, "--white-text"),
+    selected: getStyle(html, "--dark-selected")
+
 }
 
+function changeColors(colors) {
+    cards.forEach(card => {
+        card.style.background = colors.cardBg;
+        card.addEventListener('mouseover', () => {
+            card.style.background = colors.selected
+        })
+        card.addEventListener('mouseout', () => {
+            card.style.background = colors.cardBg
+        })
+    });
 
 
+    numbers.forEach(number => {
+        number.style.color = colors.cardText;
 
-const changeColors = (colors) => {
-    console.log(colors)
+    });
 
+    title.forEach(title => {
+        title.style.color = colors.cardText;
+    });
+
+    body.style.color = colors.colorText;
+    body.style.background = colors.bg;
+    topHeader.style.background = colors.topHeaderBg;
 }
 
 
 checkbox.addEventListener('change', ({ target }) => {
     target.checked ? changeColors(lightTheme) : changeColors(darkTheme)
 })
+
